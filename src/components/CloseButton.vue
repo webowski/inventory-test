26ac115fb2e5f567edff0272172a8a117d5f863f
Aleksandr Osokin <osokinweb@gmail.com>
2022-12-16 13:46:53 +0300

    Add CloseButton component

---
+++ src/components/CloseButton.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="close-button">
+		<IconCross />
+	</div>
+</template>
+
+<script setup>
+import IconCross from '@/images/icons/cross.svg'
+</script>
+
+<style lang="scss">
+.close-button {
+	width: 1.5rem;
+	height: 1.5rem;
+	position: absolute;
+	top: 0.5rem;
+	right: 0.5rem;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	cursor: var(--cursor-pointer);
+}
+
+.close-button svg path {
+	transition: fill 0.12s;
+	fill: var(--major);
+}
+
+.close-button:hover svg path {
+	fill: red;
+}
+</style>
--- src/components/Notification.vue
+++ src/components/Notification.vue
@@ -1,14 +1,12 @@
 <template>
 	<div class="notification">
 		<div class="skeleton skeleton--line"></div>
-		<div class="notification__close">
-			<IconCross />
-		</div>
+		<CloseButton />
 	</div>
 </template>
 
 <script setup>
-import IconCross from '@/images/icons/cross.svg'
+import CloseButton from './CloseButton.vue'
 </script>
 
 <style lang="scss">
@@ -19,21 +17,5 @@ import IconCross from '@/images/icons/cross.svg'
 	grid-column: span 2;
 	padding: 18px 68px 18px 18px;
 	position: relative;
-}
-
-.notification__close {
-	width: 1.5rem;
-	height: 1.5rem;
-	position: absolute;
-	top: 0.5rem;
-	right: 0.5rem;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	cursor: var(--cursor-pointer);
-}
-
-.notification__close:hover svg path {
-	fill: red;
 }
 </style>
