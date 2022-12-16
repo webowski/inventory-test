180f3502d3659c24740f1df870217c715f4a87b3
Aleksandr Osokin <osokinweb@gmail.com>
2022-12-16 10:56:24 +0300

    Add inventory grid and items

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,31 @@
-<script setup></script>
+<script setup>
+import Item from './components/Item.vue'
+
+const items = [
+	{
+		type: 1,
+		quantity: 4
+	},
+	{
+		type: 2,
+		quantity: 2
+	},
+	{
+		type: 3,
+		quantity: 5
+	}
+]
+</script>
 
 <template>
 	<div class="profile"></div>
-	<div class="inventory"></div>
+	<div class="inventory">
+		<div class="inventory__cell" v-for="(cell, index) in 25" :key="index">
+			<Item
+				:data="items[index]"
+				v-if="items.length >= cell && items.length < 25"
+			/>
+		</div>
+	</div>
 	<div class="notification"></div>
 </template>
---
+++ src/components/Item.vue
@@ -0,0 +1,60 @@
+<template>
+	<div :class="`item item--type-${data.type}`">
+		{{ data.quantity }}
+	</div>
+</template>
+
+<script setup>
+const props = defineProps({
+	data: {
+		type: Object,
+		required: true
+	}
+})
+</script>
+
+<style lang="scss">
+@import '@/styles/base/mixins';
+
+.item {
+	width: 54px;
+	height: 54px;
+	position: relative;
+
+	&::before {
+		@include pseudoBlock(48px);
+		bottom: 0;
+		left: 0;
+		background-color: #7faa65;
+	}
+
+	&::after {
+		@include pseudoBlock(48px);
+		right: 0;
+		top: 0;
+		z-index: 1;
+		background-color: rgba(184, 217, 152, 0.35);
+		backdrop-filter: blur(6px);
+	}
+}
+
+.item--type-2 {
+	&::before {
+		background-color: #aa9765;
+	}
+
+	&::after {
+		background-color: rgba(217, 187, 152, 0.35);
+	}
+}
+
+.item--type-3 {
+	&::before {
+		background-color: #656caa;
+	}
+
+	&::after {
+		background: rgba(116, 129, 237, 0.35);
+	}
+}
+</style>
